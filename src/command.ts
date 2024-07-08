9f8418302db7bbdc4348a22ab9a33d2a1edac6ed
JosiahFu <josiahfu@gmail.com>
2024-07-07 17:23:27 -0700

    Change image file name

--- src/command.ts
+++ src/command.ts
@@ -12,7 +12,7 @@ async function sendTurnMessage(interaction: ChatInputCommandInteraction, season:
         content: `## Start of ${season} ${year}`,
         files: STATE.gSlideId ? [
             new AttachmentBuilder(`https://docs.google.com/presentation/d/${STATE.gSlideId}/export?format=png`)
-                .setName('game.png')
+                .setName(`diplomacy_board_${STATE.gSlideId}_${year}_${season.toLowerCase()}.png`)
         ] : undefined,
     })
     STATE.lastEndTurn = message.id
